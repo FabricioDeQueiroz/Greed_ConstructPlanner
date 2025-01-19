050f47609a71b5f8832adaa3182edfa640b7024c
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2025-01-19 18:30:43 -0300

    Feat(API): Criando model para Agendamento
    Co-authored-by: CaioSulz <caiosulz14@gmail.com>

--- Construct Planner API/Data/AppDbContext.cs
+++ Construct Planner API/Data/AppDbContext.cs
@@ -6,5 +6,14 @@ namespace Construct_Planner_API.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Obra> Obras { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Agendamento>()
+                .HasOne(a => a.Obra)
+                .WithMany()
+                .HasForeignKey(a => a.ObraId);
+        }
     }
 }
