c59e4e40f53a58bfb8fd228f6bf05edb8a8ca3c2
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2025-01-19 23:48:26 -0300

    Feat(Frontend): Criando página de agendamento e informações.
    Co-authored-by: CaioSulz <caiosulz14@gmail.com>

---
+++ ConstructPlannerFront/Models/Agendamento.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Construct_Planner.Models;
+
+namespace ConstructPlannerFront.Models
+{
+    public class Agendamento
+    {
+        public int Id { get; init; }
+        
+        [Required]
+        public int ObraId { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataInicioProjeto { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataInicioFormas { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataInicioConcretagem { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataInicioTransporte { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataInicioMontagem { get; set; }
+        
+        [Required]
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime DataFimMontagem { get; set; }
+        
+        [NotMapped]
+        public int MaxLateness { get; set; }
+        
+        public virtual Obra? Obra { get; set; }
+    }
+}
---
+++ ConstructPlannerFront/copy-vis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p wwwroot/_content/vis-timeline
+cp node_modules/vis-timeline/dist/vis-timeline-graph2d.min.css wwwroot/_content/vis-timeline/
+cp node_modules/vis-timeline/dist/vis-timeline-graph2d.min.js wwwroot/_content/vis-timeline/
